718bbb4d0a3131b32118eeb2290d03395126ec17
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-26 19:27:42 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("--- Múltiplo ---\n");
+
+Console.Write("Digite um número...........: ");
+int nr1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Conferir se é múltiplo de..: ");
+int nr2 = Convert.ToInt32(Console.ReadLine());
+
+if (nr1 % nr2 == 0)
+
+{
+    Console.WriteLine($"\n{nr1} é múltiplo de {nr2}");
+}
+else
+{
+    Console.WriteLine($"\n{nr1} não é múltiplo de {nr2}");
+}
